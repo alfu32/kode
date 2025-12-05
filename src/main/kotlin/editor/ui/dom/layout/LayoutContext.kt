93a57104ae9945c84c11778d82398a995bcd3bcc
alfu32 <ioan.alferaru@gmail.com>
2025-12-05 01:44:02 +0100

    refactor(cleanup)

--- src/main/kotlin/editor/ui/dom/layout/LayoutContext.kt
+++
@@ -1,3 +0,0 @@
-package editor.ui.dom.layout
-
-data class LayoutContext(val cols: Int, val rows: Int)
--- src/main/kotlin/react/otherdom/TNode.kt
+++
@@ -1,6 +0,0 @@
-package react.otherdom
-
-/**
- * Placeholder node type to satisfy renderer dependencies.
- */
-data class TNode(val name: String = "")
