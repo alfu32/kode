2cd1f331c26fc0e6079e34c75571d1d598382f62
alfu32 <ioan.alferaru@gmail.com>
2025-12-08 13:11:52 +0100

    feat(recent-files): management :
    - can close/ remove files from the list
    - remembers image and bin files

---
+++ ammend.commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#!/bin/sh
+
+git filter-repo --force \
+  --email-callback '
+def callback(email):
+    return b"ioan.alferaru@gmail.com"
+' \
+  --name-callback '
+def callback(name):
+    return b"alfu32"
+'
