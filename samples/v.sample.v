1be89259deab868e5418db2d6fa9ef94b80f9fd6
alferio <ioan-cristian.alferaru@ec.europa.eu>
2025-12-17 00:11:44 +0100

    feat(code-intelligence): more packages

--- samples/v.sample.v
+++ samples/v.sample.v
@@ -1,38 +1,41 @@
 // This program displays the fibonacci sequence
 import os
 
+struct YetAnotherClassLikeStruct{
+  ve int;
+  ce string;
+}
+
 fn main() {
-	// Check for user input
-	if os.args.len != 2 {
-		println('usage: fibonacci [rank]')
+  // Check for user input
+  if os.args.len != 2 {
+    println("usage: fibonacci [rank]')
+		  return
+  }
 
-		return
-	}
+  // Parse first argument and cast it to int
+  stop := os.args[1].int()
+  // Can only calculate correctly until rank 92
+  if stop > 92 {
+    println('rank must be 92 or less')
+    return
+  }
 
-	// Parse first argument and cast it to int
-
-	stop := os.args[1].int()
-	// Can only calculate correctly until rank 92
-	if stop > 92 {
-		println('rank must be 92 or less')
-		return
-	}
-
-	// Three consecutive terms of the sequence
-	mut a := i64(0)
-	mut b := i64(0)
-	mut c := i64(1)
-	println(a + b + c)
-	for _ in 0 .. stop {
-		// Set a and b to the next term
-		a = b
-		b = c
-		// Compute the new term
-		c = a + b
-
-		// Print the new term
-		println(c)
-	}
+  // Three consecutive terms of the sequence
+  mut a := i64(0)
+  mut b := i64(0)
+  mut c := i64(1)
+  println(a + b + c)
+  for _ in 0 .. stop {
+    // Set a and b to the next term
+    a = b
+    b = c
+    // Compute the new term
+    c = a + b
+    // Print the new term
+    println(c)
+  }
 }
 
 // From https://github.com/vlang/v/blob/master/examples/fibonacci.v
+
